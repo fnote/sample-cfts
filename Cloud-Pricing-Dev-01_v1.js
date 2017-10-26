dfbdee73fdcbed14521dda66a8395b8b331dce8a
jowe6212 <jowe6212@corp.sysco.com>
2017-10-26 14:13:16 +0000

    JDO Changed MS238CPIDE02d to t2.xlarge
    git-svn-id: http://subversion.na.sysco.net/svn/CloudPricing/trunk/devops/cft@4669 53b3fdf2-bc35-43d4-91a5-3ea451ca263f

--- Cloud-Pricing-Dev-01_v1.js
+++ Cloud-Pricing-Dev-01_v1.js
@@ -1204,7 +1204,7 @@
 			"AvailabilityZone": "us-east-1d",
 			"DisableApiTermination": "false",
 			"ImageId": "ami-bd3ba0aa",
-			"InstanceType": "t2.2xlarge",
+			"InstanceType": "t2.xlarge",
 			"KeyName": { "Ref": "PemKey2" },
 			"SecurityGroupIds": [ { "Ref": "DevDBSG" },{ "Ref": "NATaccessSG" },{ "Ref": "CheckMKSG" } ],
 			"SubnetId": { "Ref": "PvtSNd" },
