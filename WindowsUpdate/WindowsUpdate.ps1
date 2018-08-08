4289911a32681390dbb127401d6ef7b0384335df
jowe6212 <jowe6212@corp.sysco.com>
2018-08-08 19:10:31 +0000

    JDO Added scripts for installing security updates on Windows Servers.
    git-svn-id: http://subversion.na.sysco.net/svn/CloudPricing/trunk/devops/cft@5343 53b3fdf2-bc35-43d4-91a5-3ea451ca263f

---
+++ WindowsUpdate/WindowsUpdate.ps1
@@ -0,0 +1,60 @@
+# WindowsUpdate.ps1
+# Run command: Powershell -File WindowsUpdate.ps1
+
+$thisComputer = $env:COMPUTERNAME
+$smtpServer = "intrelay.sysco.com"
+$smtpFrom = "Owen.James@corp.sysco.com"
+$smtpTo = "Owen.James@corp.sysco.com";
+$messageSubject = "TESTING.... " + $thisComputer + " - Update/Reboot Report"
+$Message = New-Object System.Net.Mail.mailmessage $smtpFrom, $smtpTo
+$Message.Subject = $messageSubject
+
+#Define update criteria.
+$Criteria = "IsInstalled=0 and Type='Software'"
+
+#Search for relevant updates.
+$Searcher = New-Object -ComObject Microsoft.Update.Searcher
+$SearchResult = $Searcher.Search($Criteria).Updates
+$SearchResultTitle = $Searcher.Search($Criteria).Updates | ft -a title
+$SearchResultString = $SearchResultTitle | Out-String
+
+#Download updates.
+$Session = New-Object -ComObject Microsoft.Update.Session
+$Downloader = $Session.CreateUpdateDownloader()
+$Downloader.Updates = $SearchResult
+$Downloader.Download()
+
+#Install updates.
+$Installer = New-Object -ComObject Microsoft.Update.Installer
+$Installer.Updates = $SearchResult
+$Result = $Installer.Install()
+
+#Reboot if required by updates.
+If ($Result.rebootRequired)
+{
+	$timeStamp = get-date -Format hh:mm
+	$todaysDate = get-date -format D
+	$RebootResult = "The server: " + $thisComputer + " has installed its updates and requires a reboot. 
+	It began rebooting at:" + $timeStamp + " on " + $todaysDate + "
+
+	Updates  
+	{0}" -f $SearchResultString
+	$Message.Body = $RebootResult
+	$smtp = new-Object Net.Mail.SmtpClient($smtpServer)
+	$smtp.Send($message)
+
+	shutdown.exe /t 15 /r
+}
+If (!$Result.rebootRequired)
+{
+	$timeStamp = get-date -Format hh:mm
+	$todaysDate = get-date -format D
+	$RebootResult = "The server: " + $thisComputer + " has installed its updates and did not require a reboot.
+	It finished installing its updates at:" + $timeStamp + " on " + $todaysDate + "
+
+	Updates  
+	{0}" -f $SearchResultString
+	$Message.Body = $RebootResult
+	$smtp = new-Object Net.Mail.SmtpClient($smtpServer)
+	$smtp.Send($message)
+}
