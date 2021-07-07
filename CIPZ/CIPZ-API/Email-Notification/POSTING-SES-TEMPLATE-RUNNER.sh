a18589a3e7b87e4b4cc5a5bf487bc6c32c5551a6
Chandima Samarasinghe <chandima.samarasinghe@syscolabs.com>
2021-07-07 10:11:23 +0530

    updated CIPZ-API Email template related CFTs and scripts.

--- CIPZ/CIPZ-API/Email-Notification/POSTING-SES-TEMPLATE-RUNNER.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation create-stack                                   \
-    --stack-name CP-CIPZ-API-SES-Template-Rejected-DEV                     \
-    --template-body file://CP-CIPZ-API-SES-TEMPLATE-CONFIGS.yml          \
-    --parameters    file://parameters.json
